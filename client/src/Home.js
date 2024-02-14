902a4724906e64181bca4a7636216320da3b918e
PAZHANI-KV <pazhanivel11111@gmail.com>
2024-02-14 14:49:37 +0530

    c

--- client/src/Home.js
+++ client/src/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
 
   const handleEdit = (id) => {
     axios
-      .put("http://localhost:3001/update/"+id)
+      .put("https://todo-foqz.onrender.com/update/"+id)
       .then((result) => {
         window.location.reload()
       })
@@ -25,7 +25,7 @@ const Home = () => {
 
   const handleDelete = (id) => {
     axios
-      .delete("http://localhost:3001/delete/"+id)
+      .delete("https://todo-foqz.onrender.com/delete/"+id)
       .then((result) => {
         window.location.reload()
       })
