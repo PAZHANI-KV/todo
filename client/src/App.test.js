7d93bea6ab7da33d6a9b4422e25ec481fabe8751
PAZHANI-KV <pazhanivel11111@gmail.com>
2024-02-14 14:28:38 +0530

    c

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('https://todo-foqz.onrender.com', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/https://todo-foqz.onrender.com/i);
   expect(linkElement).toBeInTheDocument();
 });
